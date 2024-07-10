df8685c67b53b5a2d66248c8eb80e4cf53eb4f6e
Gaoxz <gaoxz03@outlook.com>
2024-07-10 21:32:21 +0800

    V 2024.07.10
    添加了cmp、lsp-config等插件，更新了快捷键

--- lua/basic-init.lua
+++ lua/basic-init.lua
@@ -2,8 +2,9 @@
 vim.o.encoding = "UTF-8"
 
 -- Tab
-vim.o.tabstop = 4
+vim.o.tabstop = 8
 vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
 
 -- Numbers
 vim.o.number = true
--- lua/keymap-init.lua
+++ lua/keymap-init.lua
@@ -1,6 +1,26 @@
--- Alt + m open file tree
-vim.keymap.set("n","<A-m>", ":NvimTreeToggle<CR>",opt)
+-- Ctrl + n open file tree
+vim.keymap.set("n","<C-n>", ":NvimTreeToggle<CR>",opt)
 
 -- Ctrl + h, l change file
 vim.keymap.set("n", "<C-h>", ":BufferLineCyclePrev<CR>", opt)
 vim.keymap.set("n", "<C-l>", ":BufferLineCycleNext<CR>", opt)
+-- Ctrl + F4 close tabs
+vim.keymap.set("n", "<C-F4>", ":BufferLinePickClose<CR>", {noremap = true, silent = true})
+
+-- lsp keymap
+vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+
+-- telescope keymap
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", opt)
+vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", opt)
+vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>", opt)
+
+-- ctrl + f search
+vim.keymap.set("n", "<C-f>", "<cmd>Telescope current_buffer_fuzzy_find<CR>", opt)
+
+-- ctrl + t open terminal
+vim.keymap.set("n", "<C-t>", ":ToggleTerm<CR>", opt)
+
