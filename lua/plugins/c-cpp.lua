7b4626fc5f50f1b7e6b8ad820ce40e5b3651e2f1
Gaoxz <gaoxz03@outlook.com>
2026-01-27 00:12:44 +0800

    V 26.01.27
    Change CMake Tools setting

--- lua/plugins/c-cpp.lua
+++ lua/plugins/c-cpp.lua
@@ -8,7 +8,18 @@ return { -- clangd format to format c and cpp files
 {
     'Civitasv/cmake-tools.nvim',
     lazy = false,
+    dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
-        require("cmake-tools").setup({})
-    end
+        require("cmake-tools").setup({
+            cmake_build_directory = "build",
+            cmake_build_type = "Debug",
+            cmake_generate_options = { "-DCMAKE_EXPORT_COMPILE_COMMANDS=1" }
+        })
+    end,
+    keys = {
+        { "<leader>cg", "<cmd>CMakeGenerate<cr>", desc = "Generate" },
+        { "<leader>cb", "<cmd>CMakeBuild<cr>", desc = "Build" },
+        { "<leader>cr", "<cmd>CMakeRun<cr>", desc = "Run" },
+        { "<leader>cd", "<cmd>CMakeDebug<cr>", desc = "Debug" },
+    }
 }}
