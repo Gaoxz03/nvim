0e53d1dc7cad6ac283301e60774c2fb7e07e13b6
Gaoxz <gaoxz03@outlook.com>
2026-04-13 22:28:57 +0800

    V 26.4.13
    Change treesitter setting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {{
     build = ":TSUpdate",
     dependencies = {"nvim-lua/plenary.nvim"},
     config = function()
-        require("nvim-treesitter.configs").setup({
+        require("nvim-treesitter").setup({
             ensure_installed = {"c", "cpp", "python", "lua", "vim", "vimdoc", "javascript", "typescript", "html", "css",
                                 "json", "yaml", "toml", "markdown", "bash", "asm", "matlab", "cmake", "make", "vue",
                                 "verilog", "rust", "markdown_inline"},
